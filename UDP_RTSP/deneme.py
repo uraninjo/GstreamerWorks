53746cc91193097590b7daa2f6fcd5193167c861
uraninjo <mehmurat12@gmail.com>
2022-12-03 15:04:17 +0300

    ilkDosyalar

---
+++ UDP_RTSP/deneme.py
@@ -0,0 +1,125 @@
+import sys
+import gi
+import logging
+
+gi.require_version("GLib", "2.0")
+gi.require_version("GObject", "2.0")
+gi.require_version("Gst", "1.0")
+
+from gi.repository import Gst, GLib, GObject
+
+uri_name='127.0.0.1'
+
+def cb_newpad(decodebin, decoder_src_pad,data):
+    print("In cb_newpad\n")
+    caps=decoder_src_pad.get_current_caps()
+    gststruct=caps.get_structure(0)
+    gstname=gststruct.get_name()
+    source_bin=data
+    features=caps.get_features(0)
+
+    # Need to check if the pad created by the decodebin is for video and not
+    # audio.
+    print("gstname=",gstname)
+    if(gstname.find("video")!=-1):
+        # Link the decodebin pad only if decodebin has picked nvidia
+        # decoder plugin nvdec_*. We do this by checking if the pad caps contain
+        # NVMM memory features.
+        print("features=",features)
+        if features.contains("memory:NVMM"):
+            # Get the source bin ghost pad
+            bin_ghost_pad=source_bin.get_static_pad("src")
+            if not bin_ghost_pad.set_target(decoder_src_pad):
+                sys.stderr.write("Failed to link decoder src pad to source bin ghost pad\n")
+        else:
+            sys.stderr.write(" Error: Decodebin did not pick nvidia decoder plugin.\n")
+
+def decodebin_child_added(child_proxy,Object,name,user_data):
+    print("Decodebin child added:", name, "\n")
+    if(name.find("decodebin") != -1):
+        Object.connect("child-added",decodebin_child_added,user_data)
+
+def create_source_bin(index,uri):
+    # Create a source GstBin to abstract this bin's content from the rest of the
+    # pipeline
+    bin_name="source-bin-%02d" %index
+    print(bin_name)
+    nbin=Gst.Bin.new(bin_name)
+    if not nbin:
+        sys.stderr.write(" Unable to create source bin \n")
+
+    # Source element for reading from the uri.
+    # We will use decodebin and let it figure out the container format of the
+    # stream and the codec and plug the appropriate demux and decode plugins.
+    uri_decode_bin=Gst.ElementFactory.make("uridecodebin", "uri-decode-bin")
+    if not uri_decode_bin:
+        sys.stderr.write(" Unable to create uri decode bin \n")
+    # We set the input uri to the source element
+    uri_decode_bin.set_property("uri",uri)
+    # Connect to the "pad-added" signal of the decodebin which generates a
+    # callback once a new pad for raw data has beed created by the decodebin
+    uri_decode_bin.connect("pad-added",cb_newpad,nbin)
+    uri_decode_bin.connect("child-added",decodebin_child_added,nbin)
+
+    # We need to create a ghost pad for the source bin which will act as a proxy
+    # for the video decoder src pad. The ghost pad will not have a target right
+    # now. Once the decode bin creates the video decoder and generates the
+    # cb_newpad callback, we will set the ghost pad target to the video decoder
+    # src pad.
+    Gst.Bin.add(nbin,source,uri_decode_bin)
+    bin_pad=nbin.add_pad(Gst.GhostPad.new_no_target("src",Gst.PadDirection.SRC))
+    if not bin_pad:
+        sys.stderr.write(" Failed to add ghost pad in source bin \n")
+        return None
+    return nbin
+
+
+logging.basicConfig(level=logging.DEBUG, format="[%(name)s] [%(levelname)8s] - %(message)s")
+logger = logging.getLogger(__name__)
+
+Gst.init(sys.argv[1:])
+
+uri_decode_bin=create_source_bin(0,uri_name)
+
+videoconvert=Gst.ElementFactory.make('autovideoconvert','convert')
+
+pipeline=Gst.Pipeline.new('test-pipeline')
+
+if not uri_decode_bin or not videoconvert or not pipeline:
+    logger.error('Elementlerde sıkıntı var...')
+    sys.exit(1)
+
+pipeline.add(uri_decode_bin)
+pipeline.add(videoconvert)
+
+
+if uri_decode_bin.link(videoconvert):
+    logger.info('decodebin Bağlandı')
+
+
+
+ret=pipeline.set_state(Gst.State.PLAYING)
+
+if ret==Gst.StateChangeReturn.FAILURE:
+    logger.error("pipeline'da sıkıntı var")
+    sys.exit(1)
+
+bus = pipeline.get_bus()
+msg = bus.timed_pop_filtered(
+    Gst.CLOCK_TIME_NONE, 
+    Gst.MessageType.ERROR | Gst.MessageType.EOS)
+
+if msg:
+    if msg.type == Gst.MessageType.ERROR:
+        err, debug_info = msg.parse_error()
+        logger.error(f"Error received from element {msg.src.get_name()}: {err.message}")
+        logger.error(f"Debugging information: {debug_info if debug_info else 'none'}")
+
+    elif msg.type == Gst.MessageType.EOS:
+        logger.info("End-Of-Stream reached.")
+
+    else:
+        # This should not happen as we only asked for ERRORs and EOS
+        logger.error("Unexpected message received.")
+
+pipeline.set_state(Gst.State.NULL)
